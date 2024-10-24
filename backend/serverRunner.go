367a48561c1e1eae0aeca15a8eda87dbfb78f7a0
haalbasara <hamzaali200411@gmail.com>
2024-10-24 18:45:52 +0300

    ez

--- backend/serverRunner.go
+++ backend/serverRunner.go
@@ -8,18 +8,22 @@ import (
 
 func ServerRunner() {
 	http.HandleFunc("/", RootHandler) // this is for the home page
-	http.HandleFunc("/auth", auth) // this is for the home page
-	http.HandleFunc("/register", Register) // this is for the home page
-	http.HandleFunc("/login", Login) // this is for the home page
+	http.HandleFunc("/auth", Auth) // this is for the authentication page
+	http.HandleFunc("/register", Register) // this is for the registeration form
+	http.HandleFunc("/login", Login) // this is for the login form
 	fs := http.FileServer(http.Dir("./frontend/css"))
 	http.Handle("/frontend/css/", http.StripPrefix("/frontend/css/", fs))
 	// now we are listening for the port 8080
+	fmt.Print("The server is running on port :8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Server error:", err)
 	}
+
 }
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("You entered the main page")
+	fmt.Print(r.Method)
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -40,7 +44,8 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-	func auth(w http.ResponseWriter, r *http.Request) {
+	func Auth(w http.ResponseWriter, r *http.Request) {
+		fmt.Print("++++++++++++++++++++++++++++")  // Debug line
 		if r.Method != http.MethodGet {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
@@ -58,14 +63,47 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-
-
-
-	
-	func Login(w http.ResponseWriter, r *http.Request){
-
-	}
-
 	func Register(w http.ResponseWriter, r *http.Request){
+		if r.Method == "POST" {
+			if err := r.ParseForm(); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			username, email, password,secondPass := r.FormValue("username"),r.FormValue("email") , r.FormValue("password"), r.FormValue("secondpass")
+			if len(username) > 50 || len(email) > 50 || len(password) > 50 || len(secondPass) > 50 {
+				http.Error(w,  "Wrong number of values! you are allowed to enter up to 50 digit", http.StatusBadRequest)
+				return
+			}else if len(username) < 3 || len(email) < 8  || len(password)  < 8  || len(secondPass) < 8 {
+				http.Error(w,  "Wrong number of values! you are allowed to enter less than 8 digit", http.StatusBadRequest)
+				return
+			}
+			if username == "" || email == "" || password == "" || secondPass == ""{
+				http.Error(w, "You can not keep any field empty you must fill them all!", http.StatusBadRequest)
+				return
+			}
 
+			fmt.Printf("These are the information collected from the user:\nUserName: {%v}\nEmail: {%s}\nAnd the password: {%s}\n", username, email, password)
+			
+	}
+}
+
+	func Login(w http.ResponseWriter, r *http.Request){
+		if r.Method == "POST" {
+			if err := r.ParseForm(); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			username, password := r.FormValue("username"), r.FormValue("password")
+			if len(username) > 50 ||  len(password) > 50 ||  len(username) < 3  ||  len(password) < 8  {
+				http.Error(w, "Wrong number of values! you are allowed to enter between 8-50 digit", http.StatusBadRequest)
+				return
+			}
+			if username == "" || password == ""{
+				http.Error(w, "You can not keep any field empty you must fill them all!", http.StatusBadRequest)
+				return
+			}
+
+			fmt.Printf("These are the information collected from the user:\nUserName:{%s}\nAnd the password: {%s}\n", username,password)
+			
+	}	
 	}
