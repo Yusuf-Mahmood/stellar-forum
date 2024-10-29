972a965b61a4dc6057306d9a326d285d51bd041c
Mohamed  Ahmed <mohamedahmed0@noreply.learn.reboot01.com>
2024-10-29 14:43:41 +0000

    Update 'backend/database/DB.go'

--- backend/database/DB.go
+++ backend/database/DB.go
@@ -22,13 +22,49 @@ func InitDB() {
     createTableQueries := []string{
         `CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL,
             email TEXT NOT NULL UNIQUE,
+            username TEXT NOT NULL,
             password_hash TEXT NOT NULL,
-            cookies TEXT,
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-			);`, 
-            // will write the rest of tables later ono
+        );`,
+		`CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users (id)
+        );`,
+		`CREATE TABLE IF NOT EXISTS comments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            post_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            content TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (post_id) REFERENCES posts (id),
+            FOREIGN KEY (user_id) REFERENCES users (id)
+        );`,
+		`CREATE TABLE IF NOT EXISTS categories (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE
+        );`,
+		`CREATE TABLE IF NOT EXISTS post_categories (
+            post_id INTEGER NOT NULL,
+            category_id INTEGER NOT NULL,
+            PRIMARY KEY (post_id, category_id),
+            FOREIGN KEY (post_id) REFERENCES posts (id),
+            FOREIGN KEY (category_id) REFERENCES categories (id)
+        );`,
+		`CREATE TABLE IF NOT EXISTS likes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            post_id INTEGER NOT NULL,
+            comment_id INTEGER,
+            is_like BOOLEAN NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users (id),
+            FOREIGN KEY (post_id) REFERENCES posts (id),
+            FOREIGN KEY (comment_id) REFERENCES comments (id)
+        );`, 
     }
 
     for _, query := range createTableQueries {
